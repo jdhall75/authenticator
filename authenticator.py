2dcb85cc41d5c60fc008ad235ffc32cfe4042bce
Jason Hall <jdhall75@gmail.com>
2017-01-04 03:56:56 +0000

    syncing locations

--- authenticator.py
+++ authenticator.py
@@ -5,12 +5,7 @@ import os
 from hashlib import sha512, sha256
 import base64
 
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-sys.path.append(dir_path + '/lib')
-
-from lib.hotpie.hotpie import TOTP
+from lib.hotpie import TOTP
 
 
 b32Key = ''
