4463ec0407cc441df5abf9725ccabdafd22a42cf
Jason Hall <jdhall75@gmail.com>
2017-01-04 00:28:43 -0500

    loading keys from json file

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
-import urwid
+import json
+import sys
+from pprint import pprint
+import base64
 
+with open('keys.json') as data_file:
+    data = json.load(data_file)
 
-def exit_on_q(key):
-  if key in ('q', 'Q'):
-    raise urwid.ExitMainLoop()
+pprint(data)
 
-pallette = [
-  ('banner', 'black', 'light gray'),
-  ('streak', 'black', 'dark red'),
-  ('bg', 'black', 'dark blue'), ]
+key1 = base64.b32encode(b"test1")
+key2 = base64.b32encode(b"test2")
 
+try:
+    data['keys'][0]['key'] = key1.decode('UTF-8')
+    data['keys'][1]['key'] = key2.decode('UTF-8')
+except ValueError:
+    print("Not the right encodeing")
+    sys.exit()
 
-txt = urwid.Text(('banner', u"Hello World"), align='center')
-map1 = urwid.AttrMap(txt, 'streak')
-fill = urwid.Filler(map1)
-map2 = urwid.AttrMap(fill, 'bg')
+with open('keys.json', 'w') as data_file:
+    json.dump(data, data_file)
 
-loop = urwid.MainLoop(map2, pallette, unhandled_input=exit_on_q)
-loop.run()
